2af0b77beb382574ea0bf0d637a88b4d4eabd955
Supanut Dokmaithong <supanut@superdev.tech>
2020-04-08 14:01:58 +0700

    test gcb #4

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld msg="Welcome to SUPERDEV CO., LTD. on Staging 3" />
+    <HelloWorld msg="Welcome to SUPERDEV CO., LTD. on Staging 4" />
   </div>
 </template>
 
