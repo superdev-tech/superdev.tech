be21a65a7c834f21a3c2e50f400a7362f000c418
Supanut Dokmaithong <supanut@superdev.tech>
2020-06-08 14:15:03 +0700

    bug hot fix

--- src/plugins/vue-fb-customer-chat.js
+++ src/plugins/vue-fb-customer-chat.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueFbCustomerChat from 'vue-fb-customer-chat';
 
 Vue.use(VueFbCustomerChat, {
-  page_id: process.env.VUE_APP_CONTACTUS_API, //  change 'null' to your Facebook Page ID,
+  page_id: process.env.VUE_APP_FB_PAGE_ID, //  change 'null' to your Facebook Page ID,
   theme_color: '#0084ff', // theme color in HEX
   locale: 'th_TH', // default 'en_US'
 });
